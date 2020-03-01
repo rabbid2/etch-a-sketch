8eff79312907b0d4a2b90c26e9e9220105ecb7c2
Rabbid <elenatonkikh7@gmail.com>
2020-03-02 02:44:24 +0300

    flex элементы заменены на grid, оформлен интерфейс

--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
+const input = document.querySelector(`input`);
+const btn = document.querySelector(`button`);
+btn.addEventListener(`click`, run);
 
-const input=document.querySelector(`input`);
-const btn=document.querySelector(`button`);
-btn.addEventListener(`click`, game);
+function run(){
+    let numberOfColumn = 0;
+    clearContainer();
 
-function game(){
-    let numberOfColumn = 1;
-    if (document.querySelector(`.grid`)) document.querySelector(`.grid`).remove();
-
-    let playerValue=input.value;
-    input.placeholder=`Введите количество столбцов`;
-    if (playerValue > 200) input.placeholder=`Слишком большое число`;
-    else if (playerValue < 1 && !isNan(playerValue)) input.placeholder=`Слишком маленькое число`;
-    else if (playerValue && !isNaN(playerValue)) numberOfColumn=playerValue;
-    else input.placeholder=`Неправильный ввод`;
+    if (!validateInput(input.value)) {
+        input.placeholder = `Неправильный ввод`;
+        input.value = ``;
+    }
+    else {
+        numberOfColumn = input.value;
+        input.placeholder = `Введите количество столбцов`;
+        drawGrid(numberOfColumn);
+    }
 
     input.focus();
-    input.value=``;
-    drawGrid(numberOfColumn);
 }
 
-function drawGrid(numberOfColumn){
-    let container=document.querySelector(`.container`);
-    let grid=document.createElement(`div`);
+function validateInput(playerValue) {
+    return (playerValue && !isNaN(playerValue) && playerValue <= 300 && playerValue > 0);
+}
+
+function clearContainer() {
+    if (document.querySelector(`.grid`)) {
+        document.querySelector(`.grid`).remove();
+        document.querySelector(`.output`).remove();
+    }
+}
+
+function drawGrid(numberOfColumn) {
+    let container = document.querySelector(`.container`);
+    let grid = document.createElement(`div`);
     grid.classList.add(`grid`);
     container.appendChild(grid);
 
-    let numberOfRows=Math.round(numberOfColumn*(2/3));
-
+    let numberOfRows = Math.round(numberOfColumn * (2 / 3));
     grid.setAttribute(`style`, `grid-template-columns: repeat(${numberOfColumn}, 1fr); 
                                 grid-template-rows: repeat(${numberOfRows}, 1fr);`);
-    for(let i=1; i<=numberOfColumn*numberOfRows; i++){
-        let div=document.createElement('div');
-        div.addEventListener(`mouseover`,selectCell);
+
+    for(let i = 1; i <= numberOfColumn * numberOfRows; i++) {
+        let div = document.createElement('div');
+        div.addEventListener(`mouseover`,paintCell);
         grid.appendChild(div);
     }
 
+    createOutputInfo(numberOfColumn,numberOfRows);
 }
 
-function selectCell(e){
-    if(this.getAttribute(`style`)){
+function createOutputInfo(numberOfColumn, numberOfRows) {
+    let span = document.createElement(`span`);
+    span.classList.add(`output`);
+    span.textContent=`Количество клеток: ${numberOfColumn*numberOfRows}  (${numberOfColumn}x ${numberOfRows}y)`;
+    document.querySelector(`.header`).appendChild(span);
+}
+
+function paintCell(e) {
+    if(this.getAttribute(`style`)) {
         let colorsStr = this.getAttribute(`style`);
-        let colors= colorsStr.slice(colorsStr.indexOf(`(`)+1,colorsStr.indexOf(`)`)).split(`, `);
-        this.setAttribute(`style`,`background-color:rgb(${colors[0]-26}, ${colors[1]-26}, ${colors[2]-26});`);
+        let colors = colorsStr.slice(colorsStr.indexOf(`(`) + 1, colorsStr.indexOf(`)`)).split(`, `);
+        let coefDim = 255 * 0.1;
+        this.setAttribute(`style`, `background-color:rgb(${colors[0] - coefDim}, ${colors[1] - coefDim}, ${colors[2] - coefDim});`);
     }
-    else{
-        let colorRed=getRndInteger(0,255);
-        let colorGreen=getRndInteger(0,255);
-        let colorBlue=getRndInteger(0,255);
+    else {
+        let colorRed = getRndInteger(0, 255);
+        let colorGreen = getRndInteger(0, 255);
+        let colorBlue = getRndInteger(0, 255);
         this.setAttribute(`style`,`background-color:rgb(${colorRed}, ${colorGreen}, ${colorBlue});`);
     }
 }
